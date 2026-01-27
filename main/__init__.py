640fdbdeb4f980985474d5a6907dfa1875475f38
ICTJOURNEY <75350134+mrwhitehat6@users.noreply.github.com>
2026-01-27 12:20:08 +0530

    Update API credentials and configuration variables

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
+API_ID = config("30169419", default=None, cast=int)
+API_HASH = config("c8e9a9e23d8c3079b7173b7ff4924460
+", default=None)
+BOT_TOKEN = config("8389509300:AAFups9-XpAXPDMhsOxx_GVtdhHJyJylonM", default=None)
 SESSION = config("SESSION", default=None)
-FORCESUB = config("FORCESUB", default=None)
-AUTH = config("AUTH", default=None, cast=int)
+FORCESUB = config("saverestrictedchannel", default=None)
+AUTH = config("6283515337", default=None, cast=int)
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
