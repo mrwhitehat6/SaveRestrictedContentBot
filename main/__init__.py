4d206548dbb035c81b8d90fb48d1c65bc24353c9
ICTJOURNEY <75350134+mrwhitehat6@users.noreply.github.com>
2026-01-27 13:35:20 +0530

    Update API_ID with a new hardcoded value

--- main/__init__.py
+++ main/__init__.py
@@ -12,7 +12,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
+API_ID = config(30169419
+, default=None, cast=int)
 API_HASH = config("API_HASH", default=None)
 BOT_TOKEN = config("BOT_TOKEN", default=None)
 SESSION = config("SESSION", default=None)
